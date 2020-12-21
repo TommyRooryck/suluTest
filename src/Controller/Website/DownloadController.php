b3d082f836efc1ef636234e6d00a6a9eb6a99ff1
Tommy Rooryck <tommy_rooryck@hotmail.com>
2020-12-21 15:14:25 +0100

    initial (zonder uitbreiding)

---
+++ src/Controller/Website/DownloadController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller\Website;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+
+class DownloadController extends AbstractController
+{
+
+    /**
+     * @Route("/downloads", name="downloads")
+     */
+    public function index(): Response
+    {
+        return $this->render('pages/download.html.twig', [
+    
+        ]);
+    }
+
+    /**
+     * @Route("/downloads/excel", name="download")
+     */
+    public function downloadExcel(){
+
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setTitle("All Months");
+
+        for($x=0; $x<12; $x++){
+            $d = strtotime("+" . $x . " Months");
+            $sheet->setCellValue('A' . $x , date("F", $d));
+            $sheet->getCell('A'.$x)->setValue(date("F", $d));
+        }
+
+        $writer = new Xlsx($spreadsheet);
+
+        $filename = "months.xlsx";
+
+        $temp_file= tempnam(sys_get_temp_dir(), $filename);
+
+        $writer->save($temp_file);
+
+        return $this->file($temp_file, $filename, ResponseHeaderBag::DISPOSITION_INLINE);
+    }
+}
